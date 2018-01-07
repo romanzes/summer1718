8cc9a88b0400940a636684aa673c02be4a52fefc
Roman Petrenko <roman@canva.com>
2018-01-07 22:32:33 +1100

    better names

--- app/src/main/java/p/A.kt
+++ app/src/main/java/p/A.kt
@@ -4,42 +4,42 @@ import android.graphics.drawable.GradientDrawable
 import android.view.*
 import android.view.animation.*
 class A:android.app.Activity(){
-val d=java.util.Random()
-val L=ArrayList<View>()
-fun t(s:Int)=android.util.TypedValue.applyDimension(1,s*1f,resources.displayMetrics).toInt()
+val R=java.util.Random()
+val A=ArrayList<View>()
+fun d(s:Int)=android.util.TypedValue.applyDimension(1,s*1f,resources.displayMetrics).toInt()
 override fun onCreate(b:android.os.Bundle?){
 super.onCreate(b)
-val c=android.widget.FrameLayout(this)
-fun b()=android.widget.LinearLayout(this)
-val q=b()
-q.orientation=1
-q.gravity=17
-fun y(i:Int,j:Int)=ViewGroup.LayoutParams(i,j)
-c.addView(q,y(-1,-1))
+val F=android.widget.FrameLayout(this)
+fun L()=android.widget.LinearLayout(this)
+val c=L()
+c.orientation=1
+c.gravity=17
+fun p(w:Int,h:Int)=ViewGroup.LayoutParams(w,h)
+F.addView(c,p(-1,-1))
 fun v()=View(this)
 val p=v()
 val j=android.graphics.drawable.ShapeDrawable(android.graphics.drawable.shapes.OvalShape())
 j.paint.color=-0xeeee78
 p.background=j
-c.addView(p,y(t(20),t(20)))
-setContentView(c,y(-1,-1))
-val t=android.util.DisplayMetrics()
-windowManager.defaultDisplay.getMetrics(t)
-for(i in 0..t.heightPixels/t(10)){
-val v=b()
+F.addView(p,p(d(20),d(20)))
+setContentView(F,p(-1,-1))
+val m=android.util.DisplayMetrics()
+windowManager.defaultDisplay.getMetrics(m)
+for(i in 0..m.heightPixels/d(10)){
+val v=L()
 v.gravity=17
 val l=v()
-val m=v()
-m.background=GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT,intArrayOf(-0x222223,-0x888889))
+val o=v()
+o.background=GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT,intArrayOf(-0x222223,-0x888889))
 val r=v()
-v.addView(l,y(t(10),t(10)))
-v.addView(m,y(t(100),t(3)))
-v.addView(r,y(t(10),t(10)))
-fun b()=d.nextInt(256)
+v.addView(l,p(d(10),d(10)))
+v.addView(o,p(d(100),d(3)))
+v.addView(r,p(d(10),d(10)))
+fun b()=R.nextInt(256)
 l.setBackgroundColor(argb(255,b(),b(),b()))
 r.setBackgroundColor(argb(255,b(),b(),b()))
-q.addView(v,y(t(120),-2))
-L.addAll(listOf(l,m,r))
+c.addView(v,p(d(120),-2))
+A.addAll(listOf(l,o,r))
 val a=android.animation.ValueAnimator.ofFloat(1f,-1f)
 a.addUpdateListener{
 v.scaleX=it.animatedValue as Float
@@ -51,13 +51,12 @@ a.currentPlayTime=i*100L
 a.start()
 }
 fun u(){
-val P=2
-val a=TranslateAnimation(P,2f,P,-.5f,P,d.nextFloat(),P,d.nextFloat())
+val a=TranslateAnimation(2,2f,2,-.5f,2,R.nextFloat(),2,R.nextFloat())
 a.duration=2000
 p.startAnimation(a)
 p.postDelayed({
 u()
-if(L.size>0)L.removeAt(d.nextInt(L.size)).visibility=4
+if(A.size>0)A.removeAt(R.nextInt(A.size)).visibility=4
 },2000)
 }
 u()
